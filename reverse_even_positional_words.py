e4d71bcd8586b093a9aa059c29f1e2c98ce6b0d4
Bveerabhadrarao <99869311+Bveerabhadrarao@users.noreply.github.com>
2023-05-26 20:44:01 +0530

    n=input().split() s="" for i in range(len(n)):     if i%2==0:         i=list(n[i])         r=""         for k in i:             r=k+r         s=s+" "+r     else:         s=s+" "+n[i] print(s[1:])

---
+++ reverse_even_positional_words.py
@@ -0,0 +1,12 @@
+n=input().split()
+s=""
+for i in range(len(n)):
+    if i%2==0:
+        i=list(n[i])
+        r=""
+        for k in i:
+            r=k+r
+        s=s+" "+r
+    else:
+        s=s+" "+n[i]
+print(s[1:])
