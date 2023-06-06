faa223d5d834cd37b9a76a32bb773f04c50d3e84
Bveerabhadrarao <99869311+Bveerabhadrarao@users.noreply.github.com>
2023-06-06 10:44:50 +0530

    n1=int(input()) l1=list(map(int,input().split())) n2=int(input()) l2=list(map(int,input().split())) t=int(input()) c=0 for i in range(len(l1)):     for j in range(l1[i],l2[i]+1):         if j==t:             c+=1 print(c)

---
+++ _Number_of_Students_Doing_Homework_at_a_Given_Time.py
@@ -0,0 +1,11 @@
+n1=int(input())
+l1=list(map(int,input().split()))
+n2=int(input())
+l2=list(map(int,input().split()))
+t=int(input())
+c=0
+for i in range(len(l1)):
+    for j in range(l1[i],l2[i]+1):
+        if j==t:
+            c+=1
+print(c)
